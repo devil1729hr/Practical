434dd9fedfb4fb99209e65fb7a8bcaef7d4e0cdc
devil1729hr <asankit82@gmail.com>
2022-06-22 13:53:09 +0530

    Two Programs (22-06-2022)

---
+++ Cjava.java
@@ -0,0 +1,6 @@
+class Cjava{
+    public static void main(String[]args){
+	System.out.println("Course: B.Tech");
+    System.out.println("Stream: Electronics and Communication Engineering");
+}
+}
---
+++ Sumjava.java
@@ -0,0 +1,18 @@
+class Sumjava{
+    public static void main(String[]args){
+        int num1 = 4;
+        int num2 = 3;
+        int sum = num1 + num2;
+        int sub = num1 - num2;
+        int div = num1 / num2;
+        int mod = num1 % num2;
+        int mult = num1 * num2;
+
+	    System.out.println("Sum of 4 and 3 is " + " " +  sum);
+        System.out.println("Sub 4 - 3 is" + " " + sub);
+        System.out.println("Mult 4 * 3 is" + " " + mult);
+        System.out.println("Mod of 4 % 3" + " " + mod);
+        System.out.println("Div of 4 / 3" + " " + div);
+   
+}
+}
